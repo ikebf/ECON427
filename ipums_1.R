b5d747f7111242bc81b59049522b3d784aac28d2
ikebf <89275242+ikebf@users.noreply.github.com>
2024-01-07 16:43:36 -0500

    Update ipums_1.R

--- ipums_1.R
+++ ipums_1.R
@@ -1,11 +1,14 @@
-# Set working directory
+# Clear memory
+rm(list = ls())
+
+# Set working directory (customize this step with an appropriate folder)
 setwd('C:/Users/lohi/OneDrive - UNC-Wilmington/Documents/R/427')
 
-# Installing packages
-install.packages('ipumsr')
-install.packages('dplyr')
-install.packages('purrr')
-install.packages('ggplot2')
+# Installing packages (do this only once)
+# install.packages('ipumsr')
+# install.packages('dplyr')
+# install.packages('purrr')
+# install.packages('ggplot2')
 
 # Loading packages
 library(ipumsr)
@@ -13,8 +16,8 @@ library(dplyr)
 library(purrr)
 library(ggplot2)
 
-# Save key in .Renviron for use across sessions
-set_ipums_api_key("59cba10d8a5da536fc06b59d5068ea8d49f3425282bdb16a9eef8d7d", save = TRUE)
+# Save key in .Renviron for use across sessions (try to get your own API key!)
+# set_ipums_api_key("59cba10d8a5da536fc06b59d5068ea8d49f3425282bdb16a9eef8d7d", save = TRUE)
 
 # Note: sample codes are here https://cps.ipums.org/cps-action/samples/sample_ids
 ?define_extract_cps
@@ -27,7 +30,7 @@ namesdf = get_sample_info(collection = 'cps') %>%
               filter(year >= 1964) %>%
               pull(name)
 
-# Extracting data from IPUMS
+## Extracting data from IPUMS
 cps_extract_request = define_extract_cps(
   description = "1964-2023 CPS Data",
   samples = namesdf,
@@ -42,17 +45,24 @@ cps_data = read_ipums_micro(data_files)
 
 ## Analyzing data (Exercise 1)
 # Data selection
-df = cps_data %>% filter(AGE >= 21 & AGE <=30 & INCWAGE > 0 & INCWAGE < 99999998) %>%
+df_1 = cps_data %>% filter(AGE >= 21 & AGE <=30 & INCWAGE > 0 & INCWAGE < 99999998) %>%
         filter(EDUC %in% c(70,71,72,73,110,111)) %>%
         filter(ASECWT >= 0)
 
+# Save the data
+save(df_1, file = 'df_1.RData')
+
+# Load data
+load('df_1.RData')
+
 # Creating variables
-df = df %>% mutate(college = ifelse(EDUC %in% c(110, 111), 1, 0)) %>%
+df = df_1 %>% mutate(college = ifelse(EDUC %in% c(110, 111), 1, 0)) %>%
       mutate(lwage = log(INCWAGE)) %>%
       mutate(college = as.factor(college))
 
+## Plotting the data
 wage_table = df %>% group_by(YEAR, college) %>%
-              summarize(avg_wage = mean(lwage, na.rm = TRUE))
+              summarize(avg_wage = weighted.mean(lwage, ASECWT, na.rm = TRUE))
 
 wage_table %>% ggplot(aes(x = YEAR, y = avg_wage, group = college, color = college)) +
   geom_line() +
