d711f6cbd0158307e16c3989edf5fc33ae2d1f04
ikebf <89275242+ikebf@users.noreply.github.com>
2024-01-24 15:57:39 -0500

    Update ipums_2.R

--- ipums_2.R
+++ ipums_2.R
@@ -15,14 +15,14 @@ namesdf = get_sample_info(collection = 'cps') %>%
   filter(grepl('ASEC', description) & !grepl('Research', description)) %>%
   mutate(year = substr(description, nchar(description) - 4 + 1, nchar(description))) %>% 
   mutate(year = as.numeric(year)) %>%
-  filter(year >= 1968) %>%
+  filter(year >= ...) %>% ### FILL THIS IN WITH THE APPROPRIATE STARTING YEAR ###
   pull(name)
 
 ## Extracting data from IPUMS
 cps_extract_request = define_extract_cps(
   description = "1968-2023 CPS Data",
   samples = namesdf,
-  variables = c("YEAR", "SEX", "AGE", "LABFORCE", "NCHLT5", "ASECWT")
+  variables = c("YEAR", "SEX", "AGE", ...) ### COMPLETE THIS LIST WITH THE APPROPRIATE VARIABLES ###
 )
 
 submitted_extract = submit_extract(cps_extract_request)
@@ -33,7 +33,7 @@ cps_data = read_ipums_micro(data_files)
 
 ## Analyzing data (Exercise 1)
 # Data selection
-df_2 = cps_data %>% filter(AGE >= 20 & AGE <= 50 & LABFORCE != 0)
+df_2 = cps_data %>% filter(AGE >= ... & AGE <= ... & LABFORCE != 0) ### FILL IN THE AGE RANGES TO CONSIDER ###
 
 # Save the data
 save(df_2, file = 'df_2.RData')
